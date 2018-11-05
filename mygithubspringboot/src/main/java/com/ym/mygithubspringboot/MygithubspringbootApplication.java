6884c053ffc62620dd6641d5be14051c3efb6b27
睡觉~zZ <1134270009@qq.com>
2018-11-05 21:08:35 +0800

    first commit

---
+++ mygithubspringboot/src/main/java/com/ym/mygithubspringboot/MygithubspringbootApplication.java
@@ -0,0 +1,12 @@
+package com.ym.mygithubspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygithubspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MygithubspringbootApplication.class, args);
+    }
+}
---
+++ mygithubspringboot/src/test/java/com/ym/mygithubspringboot/MygithubspringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ym.mygithubspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MygithubspringbootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
